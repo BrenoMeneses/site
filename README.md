d6fbbbb8e798aca5116adf954b1efc8cf50e241b
BrenoMeneses <brenomeneses525@gmail.com>
2022-08-15 19:08:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# site
+ tentando copiar um site 
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class=""header1>
+        <div class="menu">
+            <nav>
+                <div class="botaoPesquisa">
+                    <ul>
+                        <li>
+                            <a href="#" class="linkMenu">pesquisa</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <img src="imagens/cropped-logo-novo-1-1.webp" alt="">
+            <nav>
+                <div class="botaoMenu">
+                    <ul>
+                        <li>
+                            <a href="#" class="linkMenu">menu</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <h1>Jornal Tulmulto</h1>
+        <img src="imagens/cropped-cabecalho-site-2.png" alt="" class="imagem2">
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
